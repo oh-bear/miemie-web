b3ca03c05b6a9225254285eb5b1a763e539ed89f
Airing <gx-deng@163.com>
2017-01-28 21:12:59 +0800

    create index.html
    1. 创建首页
    2. 实现登录与注册界面（有bug待解决）
    3. 使用wilddog实现注册与登录功能

---
+++ static/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    var config = {
+        authDomain: "miemie.wilddog.com"
+    };
+    wilddog.initializeApp(config);
+});
+$(function () {
+    $(".typed").typed({
+        strings: ["这里是咩咩单词 MIE~", "MIEMIE BY AIRING"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 700
+    });
+});
+
+// Multiple Modals 无效
+$('.united.modal')
+    .modal({
+        allowMultiple: false
+    })
+;
+$('#modal2').modal('attach events', '#btn-modal-1');
+
+
+function start() {
+    $('#modal1.modal').modal('show');
+}
+
+// 临时 debug 注册事件
+function showSignUp() {
+    $('#modal2.modal').modal('show');
+}
+
+function signIn() {
+    var email = $('#signInEmail').val();
+    var pwd = $('#signInPassword').val();
+    console.log(email)
+    wilddog.auth().signInWithEmailAndPassword(email, pwd)
+        .then(function () {
+            console.info("login success, currentUser->", wilddog.auth().currentUser);
+        }).catch(function (err) {
+        console.info('login failed ->', err);
+    });
+}
+
+function signUp() {
+
+    var email = $('#signUpEmail').val();
+    var pwd = $('#signUpPassword').val();
+
+    wilddog.auth().createUserWithEmailAndPassword(email, pwd)
+        .then(function (user) {
+            console.info("user created.", user);
+        }).catch(function (err) {
+        console.info("create user failed.", err);
+    });
+}
