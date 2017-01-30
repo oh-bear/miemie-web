2ae8033d437cec3c9476bc0729099f4401959b91
Airing <gx-deng@163.com>
2017-01-30 12:20:39 +0800

    create app/index.html
    实现app首页顶部菜单

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
+    // 初始化 wilddog
     var config = {
         authDomain: "miemie.wilddog.com"
     };
     wilddog.initializeApp(config);
+    
+    $('.ui.menu .ui.dropdown').dropdown({
+        on: 'hover'
+    });
+    $('.ui.menu a.item').on('click', function() {
+        $(this).addClass('active')
+        .siblings()
+        .removeClass('active');
+    });
 });
-$(function () {
-    $(".typed").typed({
+
+/**
+ * 首页标题打字效果  
+ */
+ $(function () {
+    $(".site-header .typed").typed({
         strings: ["这里是咩咩单词 MIE~", "MIEMIE BY AIRING"],
         typeSpeed: 30,
         backSpeed: 30,
@@ -13,45 +27,59 @@ $(function () {
     });
 });
 
-// Multiple Modals 无效
-$('.united.modal')
-    .modal({
-        allowMultiple: false
-    })
-;
-$('#modal2').modal('attach events', '#btn-modal-1');
-
-
-function start() {
+/**
+ * 首页开始按钮点击事件，显示登录弹窗  
+ */
+ function start() {
     $('#modal1.modal').modal('show');
 }
 
-// 临时 debug 注册事件
-function showSignUp() {
+/**
+ * 首页显示注册弹窗  
+ */
+ function showSignUp() {
     $('#modal2.modal').modal('show');
 }
 
-function signIn() {
+/**
+ * 用户登录  
+ */
+ function signIn() {
     var email = $('#signInEmail').val();
     var pwd = $('#signInPassword').val();
     console.log(email)
     wilddog.auth().signInWithEmailAndPassword(email, pwd)
-        .then(function () {
-            console.info("login success, currentUser->", wilddog.auth().currentUser);
-        }).catch(function (err) {
+    .then(function () {
+        console.info("login success, currentUser->", wilddog.auth().currentUser);
+        location.href = "./app/index.html";
+    }).catch(function (err) {
         console.info('login failed ->', err);
     });
 }
 
-function signUp() {
-
+/**
+ * 用户注册
+ */
+ function signUp() {
     var email = $('#signUpEmail').val();
     var pwd = $('#signUpPassword').val();
-
     wilddog.auth().createUserWithEmailAndPassword(email, pwd)
-        .then(function (user) {
-            console.info("user created.", user);
-        }).catch(function (err) {
+    .then(function (user) {
+        console.info("user created.", user);
+    }).catch(function (err) {
         console.info("create user failed.", err);
     });
 }
+
+
+/**
+ * 学习页标题打字效果  
+ */
+ $(function () {
+    $(".menu-title .typed").typed({
+        strings: ["这里是咩咩单词 MIE~", "MIEMIE"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 700
+    });
+});
